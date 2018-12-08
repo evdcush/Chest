da0ee5c52c56bae13e605e12a026869429f38acc
Evan Cushing <evdcush@protonmail.com>
2018-12-08 09:54:32 -0800

    minor edits func

--- Scripts/get_readme.py
+++ Scripts/get_readme.py
@@ -5,21 +5,24 @@ import sys
 import subprocess as sbp
 
 # dest dirs
-HOME = os.environ['HOME']
-dest    = f'{HOME}/.Nextcloud/READMEs'
-ln_dest = f'{HOME}/Documents/READMEs'
+dest = f"{os.environ['HOME']}/.Nextcloud/READMEs"
 
 def wget_readme(url, dest=dest):
     # README wget formatting
     readme_fname = f'{url.split("/")[-1]}.md'
     u = url.replace('github', 'raw.githubusercontent', 1) + '/master/README.md'
+    fpath = f'{dest}/{readme_fname}'
+    if os.path.exists(fpath):
+        print(f'DUPLICATE; existent {readme_fname}')
+        sbp.run(f"mv {fpath} {fpath[:-3] + '.prev.md'}", shell=True)
 
     # wget and symlink
-    sbp.run(f'wget {u} -O {dest}/{readme_fname}', shell=True)
-    sbp.run(f'ln -sf {dest}/{readme_fname} {ln_dest}', shell=True)
+    sbp.run(f'wget {u} -O {fpath}', shell=True)
 
 if __name__ == '__main__':
     URL = sys.argv[1]
-    wget_readme(URL)
+    if len(sys.argv) > 2:
+        dest = sys.argv[2]
+    wget_readme(URL, dest)
 
 
