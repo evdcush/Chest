517f5dec471745c39172442e05670566d329aa1e
Evan Cushing <evdcush@protonmail.com>
2018-12-09 20:48:52 -0800

    update to get_rdm script

--- Scripts/get_readme.py
+++ Scripts/get_readme.py
@@ -5,24 +5,64 @@ import sys
 import subprocess as sbp
 
 # dest dirs
-dest = f"{os.environ['HOME']}/.Nextcloud/READMEs"
+HOME = os.environ['HOME']
+readme_dest = f"{HOME}/.Nextcloud/READMEs"
+file_dest = f'{HOME}/Projects'
 
-def wget_readme(url, dest=dest):
-    # README wget formatting
-    readme_fname = f'{url.split("/")[-1]}.md'
-    u = url.replace('github', 'raw.githubusercontent', 1) + '/master/README.md'
-    fpath = f'{dest}/{readme_fname}'
-    if os.path.exists(fpath):
-        print(f'DUPLICATE; existent {readme_fname}')
-        sbp.run(f"mv {fpath} {fpath[:-3] + '.prev.md'}", shell=True)
+def check_dup(file_path):
+    if os.path.exists(file_path):
+        print(f'Duplicate existent for {file_path}\nMoving previous to .cache')
+        sbp.run(f'mv {file_path} {HOME}/.cache', shell=True)
+
+
+def wget_raw(url, fname, dest=file_dest):
+    # Format endpoints
+    u = url.replace('github', 'raw.githubusercontent', 1) + f'/master/{fname}'
+    fpath = f'{dest}/{fname}'
+    check_dup(fpath)
 
     # wget and symlink
     sbp.run(f'wget {u} -O {fpath}', shell=True)
 
-if __name__ == '__main__':
-    URL = sys.argv[1]
-    if len(sys.argv) > 2:
-        dest = sys.argv[2]
-    wget_readme(URL, dest)
+# Potential readme file names:
+rdme = ['README.md', 'README', 'README.rst', 'README.txt',
+        'readme.md', 'readme', 'readme.txt']
 
+def wget_readme(url, dest=readme_dest):
+    chk_exist = lambda u: sbp.run(f'wget --spider {u}', shell=True)
+
+    # URL formatting
+    u = url.replace('github', 'raw.githubusercontent', 1) + '/master/'
+    fname = f'{url.split("/")[-1]}'
+
+    # Check for filename format
+    correct_rdme_name = ''
+    for rdm in rdme:
+        if chk_exist(u + rdm).returncode == 0:
+            correct_rdme_name = rdm
+            break
+    if correct_rdme_name == '':
+        print('readme does not match standard readme file names or does not exist')
+    else:
+        # Get extension of correct readme
+        ext = correct_rdme_name.split('.')[-1]
+        if ext != correct_rdme_name:
+            fname += '.' + ext
+        #=== append true readme name to url
+        u = u + correct_rdme_name
+        #=== append file write name to dest
+        dest = f'{dest}/{fname}'
+        check_dup(dest)
+        #=== Get readme
+        sbp.run(f'wget {u} -O {dest}', shell=True)
+
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    URL = args.pop()
+    if args:
+        # args is [filename, dest] or [filename]
+        wget_raw(URL, *args)
+    else:
+        wget_readme(URL)
 
