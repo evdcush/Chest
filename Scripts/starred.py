27e435a754aed108cfd03c799923f2485347c492
Evan Cushing <evdcush@protonmail.com>
2018-12-11 11:22:01 -0800

    modifying starred for  more info of interest

--- Scripts/starred.py
+++ Scripts/starred.py
@@ -35,15 +35,36 @@ desc = ("# :stars: Star Map\n"
 
 CC0_button = ('http://mirrors.creativecommons.org'
              '/presskit/buttons/88x31/svg/cc-zero.svg')
+LINE_BREAK = '\n\n---\n\n'
+ffi.write("\n")
+        # repo stars
+        for lng, projs in sorted(data.items(), key=lambda data: data[0]):
+            ffi.write(f"# {lng} \n\n")
+            ffi.write("---\n\n")
+            for proj in projs:
+                pushed_at = datetime.strptime(
+                    proj['pushed_at'], "%Y-%m-%dT%H:%M:%SZ")
 
-license_ = ("## Licence\n"
+                last_code_push = str((datetime.utcnow() - pushed_at).days)
+
+                ffi.write(f"## {proj.get('name').strip()}\n\n")
+                ffi.write(f"```\n{proj.get('description')}\n```\n\n")
+                ffi.write(f"  * [Github]({proj['html_url']})\n")
+                ffi.write(f"  * Stars: {proj['stargazers_count']}\n")
+                #ffi.write(f"  * Open issues: {proj['open_issues_count']}\n")
+                ffi.write("  * Last pushed: {} ({} days)\n\n".format(
+                    pushed_at.strftime("%Y-%m-%d"), last_code_push))
+
+license_ = (LINE_BREAK
+            "## Licence\n"
             f"[![CC0]({CC0_button})]"
             "(https://creativecommons.org/publicdomain/zero/1.0/)\n"
             "This work is released in the public domain; "
-            "no copyright or related rights are reserved over this work\n\n"
+            "no copyright or related rights are reserved over this work"
+            LINE_BREAK
             "## Acknowledgement\n"
             "*Original generator script from "
-            "[maguowei's starred](https://github.com/maguowei/starred)*")
+            "[maguowei's starred.py](https://github.com/maguowei/starred)*")
 
 # HTML formatting
 # ---------------
@@ -116,9 +137,9 @@ def starred(user, token, sort_results=False):
     print(license_)
 
 if __name__ == '__main__':
-    args = vars(P.parse_args())
-    user  = args['username']
-    token = args['token']
-    sort_results = args['sort']
+    args  = P.parse_args()
+    user  = args.username
+    token = args.token
+    sort_results = args.sort
     starred(user, token, sort_results)
 
