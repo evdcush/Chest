9f95dca3ffd8d5eafad5458c5d12b0e241aef0f3
Evan Cushing <evdcush@protonmail.com>
2019-02-06 22:48:32 -0800

    script for cat files togeth, incomplete

---
+++ Scripts/concat_src_files.py
@@ -0,0 +1,32 @@
+from glob import glob
+
+task_dirs  = {d.split('/')[-1]: [] for d in glob('common-tasks/*')}
+task_fpaths = glob('common-tasks/*/*')
+
+for fp in task_fpaths:
+    key = fp.split('/')[1]
+    task_dirs[key].append(fp)
+
+concatted_fname = 'common_tasks.cpp'
+
+
+line = '//' + '='*76 + '//'
+
+soft_line = "\n// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n\n"
+
+def get_header_line(topic):
+    header = f"\n\n{line}\n// {topic:^74} //\n{line}\n\n"
+    return header
+
+
+def write_file():
+    with open(concatted_fname, 'w') as wfile:
+        for k, v in task_dirs.items():
+            cur_header = get_header_line(k)
+            wfile.write(cur_header)
+            for fpath in v:
+                with open(fpath) as cpp_file:
+                    for line in cpp_file.read():
+                        wfile.write(line)
+                wfile.write(soft_line)
+
