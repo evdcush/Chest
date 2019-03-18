1f9db96f4c0bcaebfe6914b6f8f133ed4fa8e465
Evan Cushing <evdcush@protonmail.com>
2019-03-17 22:29:58 -0700

    cheats, notes

--- Resources/Templates/template.py
+++ Resources/Templates/template.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os
 import sys
 import pdb
 import code
@@ -7,10 +8,15 @@ import traceback
 from pprint import pprint
 import numpy as np
 
+# Snippets
+# ========
+file_path = os.path.abspath(os.path.dirname(__file__))
+path_up1 = '/'.join(file_path.split('/')[:-1])
+if not path_up1 in sys.path:
+    sys.path.append(path_up1)
 
-# snips
-# =====
 #code.interact(local=dict(locals(), **globals()))
+
 class AttrDict(dict):
     __getattr__ = dict.__getitem__
     __setattr__ = dict.__setitem__
@@ -20,16 +26,61 @@ class AttrDict(dict):
 # parser
 # ======
 CLI = argparse.ArgumentParser()
-add_arg = CLI.add_argument
+
+# Add arguments here preceding any subparser
+
+# subparsers
+#subparsers = CLI.add_subparsers(dest='subcmd')
+subparsers = None
+
+# Subcommand decorator
+def argp(*names_or_flags, **kwargs): return names_or_flags, kwargs
+
+def subcmd(*parser_args):
+    """ decorator used to define args to a subcommand """
+    global subparsers
+    if subparsers is None:
+        subparsers = cli.add_subparsers(dest='subcmd')
+    parent = subparsers
+    def decorator(func):
+        parser = parent.add_parser(func.__name__, description=func.__doc__)
+        for args, kwargs in parser_args:
+            parser.add_argument(*args, **kwargs)
+        parser.set_defaults(func=func)
+    return decorator
+
+# parser args
+CLI.add_argument('-f', '--foo', type=str, nargs='?', default=None, const='baz',
+    help="sample 'optional with default' primary parser arg")
+
+@subcmd(argp('-m', '--model', type=str, metavar='path', help='path/to/model'))
+def pretrained(args):
+    """ use pretrained model params """
+    mpath = args.model
+    if not os.path.exists(mpath):
+        CLI.error('Path to model not found!')
+    train.train(from_params=mpath)
 
 
 def main():
+    args = CLI.parse_args()
+    subcommand = args.subcmd
+
+    if args.foo is not None:
+        foo = args.foo
+        if foo != 'baz':
+            print('foobar')
+        else:
+            print('baz')
+
+    if subcommand is not None:
+        CLI.func(args)
+
     # some WIP code that maybe raises an exception
     raise BaseException("oh no, exception!")
     return 0
 
 if __name__ == "__main__":
-    #args = CLI.parse_args()
     try:
         ret = main()
     except:
--- Scripts/utils.py
+++ Scripts/utils.py
@@ -1,13 +1,244 @@
 import os
 import sys
 import code
+import argparse
 from functools import wraps
 import yaml
+
+class AttrDict(dict):
+    __getattr__ = dict.__getitem__
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+#-----------------------------------------------------------------------------#
+#                                   Parser                                    #
+#-----------------------------------------------------------------------------#
+
+# Parser
+# ======
+CLI = argparse.ArgumentParser()
+#subparsers = CLI.add_subparsers(dest='subcmd')
+subparsers = None
+
+# Subcommands
+# ===========
+def subcmd(*args, **kwargs):
+    """Decorator to define a new subcommand in a sanity-preserving way.
+
+    The function will be stored in the ``func`` variable when the parser
+    parses arguments so that it can be called directly like so::
+        args = cli.parse_args()
+        args.func(args)
+
+    Usage example::
+        @subcmd(argp("-d", help="Enable debug mode", action="store_true"))
+        def foo(args):
+            print(args)
+
+    Then on the command line::
+        $ python cli.py foo -d
+    """
+    global subparsers
+    if subparsers is None:
+        subparsers = cli.add_subparsers(dest='subcmd')
+    parent = subparsers
+    def decorator(func):
+        parser = parent.add_parser(func.__name__, description=func.__doc__)
+        #for args, kwargs in parser_args:
+        #    parser.add_argument(*args, **kwargs)
+        parser.add_argument(*args, **kwargs)
+        parser.set_defaults(func=func)
+    return decorator
+
+
+"""
+# SAMPLES
+@subcmd()  # INVALID with updated subcmd
+def nothing(args):
+    print("Nothing special!")
+
+@subcmd(argp('-d', help='debug mod', action='store_true'))
+def test(args):
+    print(args)
+
+@subcmd(argp('-f', '--filename', help="A thing with a filename"))
+def filename(args):
+    print(args.filename)
+
+@subcmd(argp('name', help='Name'))
+def name(args):
+    print(args.name)
+"""
+
+
 
 #-----------------------------------------------------------------------------#
 #                                Handy Helpers                                #
 #-----------------------------------------------------------------------------#
 
+# yaml rw helpers
+# ===============
+def R_yml(fname):
+    with open(fname) as file:
+        return yaml.load(file)
+
+def W_yml(fname, obj):
+    with open(fname, 'w') as file:
+        yaml.dump(obj, file, default_flow_style=False)
+
+
+# Decorators
+# ==========
+def NOTIMPLEMENTED(f):
+    """ Like TODO, but for functions in a class
+        raises error when wrappee is called """
+    @wraps(f)
+    def not_implemented(*args, **kwargs):
+        func_class = args[0]
+        f_class_name = func_class.get_class_name()
+        f_name = f.__name__
+        msg = '\n  Class: {}, function: {} has not been implemented!\n'
+        print(msg.format(f_class_name, f_name))
+        raise NotImplementedError
+    return not_implemented
+
+
+#-----------------------------------------------------------------------------#
+#                                    PATHS                                    #
+#-----------------------------------------------------------------------------#
+
+###  BASE LEVEL  ###
+HOME  = os.environ['HOME']
+
+# Dot dirs
+# ========
+DOTS = f"{HOME}/.Dots"
+APPS = f"{HOME}/.Apps"
+BIN  = f"{HOME}/.local/bin"
+DESKTOPS = f"{HOME}/.local/share/applications"
+
+#==== Home dirs
+CHEST = f"{HOME}/Chest"
+CLOUD = f"{HOME}/Cloud"
+PROJECTS  = f"{HOME}/Projects"
+DOCUMENTS = f"{HOME}/Documents"
+
+#==== .apps
+ICONS = f"{APPS}/Icons"
+BINARIES = f"{APPS}/Binaries"
+NATIVEFIED = f"{APPS}/Nativefied"
+
+
+#==== Cloud
+READMES = f"{CLOUD}/READMEs"
+AWESOME_LISTS = READMES + "/awesome_lists"
+RESOURCES_CLOUD = f"{CLOUD}/Resources"
+DOCUMENTS_CLOUD = f"{CLOUD}/Reading"
+
+#==== Chest
+DOTS_CHEST = f"{CHEST}/Dots"
+RESOURCES_CHEST = f"{CHEST}/Resources"
+TEMPLATES_CHEST = RESOURCES_CHEST + "/Templates"
+LICENSES_CHEST  = TEMPLATES_CHEST + "/Licenses"
+
+#==== Projects
+HOARD = f"{PROJECTS}/Hoard"
+HOARD_ARCHIVE = HOARD + "/Archive"
+PAPER = f"{PROJECTS}/DocHub/Literature"
+
+# Files
+# =====
+inbox_read_path = f"{CLOUD}/Reading/inbox.txt"
+
+# Tokens
+api_tokens_path = f"{RESOURCES_CLOUD}/api_tokens.yml"
+zot_token = lambda: R_yml(api_tokens_path)['zotero']
+public_gh_tokens = lambda: R_yml(api_tokens_path)['github']
+
+# Licenses
+LICENSES = AttrDict(
+    bsd3=LICENSES_CHEST + '/BSD-3-Clause.txt',
+    lgpl=LICENSES_CHEST + '/LGPL-3.0.txt',
+    agpl=LICENSES_CHEST + '/AGPL-3.0.txt',
+    ccsa=LICENSES_CHEST + '/CC-BY-NC-SA-4.0.txt',
+    ccnd=LICENSES_CHEST + '/CC-BY-NC-ND-4.0.txt',
+    )
+
+def add_to_read_inbox(entry):
+    with open(inbox_read_path, 'a') as ibx:
+        ibx.write(entry + '\n')
+
+#-----------------------------------------------------------------------------#
+#                                  Hoarding                                   #
+#-----------------------------------------------------------------------------#
+
+# Hoard vars
+# ==========
+# Paths
+hoard_inbox_path   = f"{HOARD}/inbox.yml"
+hoard_archive_path = f"{HOARD}/archive.yml"
+
+# Inbox/archive mapping
+hoard_inbox_keys = {'o': 'orgs', 'r': 'repos', 'u':'users'}
+
+# smelly mapping: 'files' keys are mapped in somewhat opposite manner
+#  to inbox keys, but it makes the archiving func a little cleaner
+hoard_archive_keys = {'awesome_lists': 'files',
+                      'readmes': 'files',
+                      'src': 'files',
+                      'gists': 'files',
+                      **hoard_inbox_keys}
+
+# Hoard funcs
+# ===========
+def init_github():
+    """ initialize github sess """
+    import github3
+    tokens = public_gh_tokens()
+    hoard_token = tokens['hoard']['token']
+    github = github3.login(token=hoard_token)
+    return github
+
+
+# Hoard inboxing
+def add_to_inbox(key, entry):
+    assert key in hoard_inbox_keys
+    key = hoard_inbox_keys[key]
+    hoard = R_yml(hoard_inbox_path)
+    hoard[key].append(entry)
+    W_yml(hoard_inbox_path, hoard)
+
+# Hoard archive
+def add_to_archive(key, entry):
+    """
+    Two types of indexing
+    add_to_archive('src', <entry>)
+      key == 'src'; hoard_key == 'files'
+      hoard_archive[hoard_key][key] == hoard_archive['files']['src']
+
+    add_to_archive('r', <entry>)
+      key == 'r'; hoard_key == 'repos';
+      hoard_archive[hoard_key]
+
+    """
+    assert key in hoard_archive_keys
+    hoard_key = hoard_archive_keys[key]
+    hoard_archive = R_yml(hoard_archive_path)
+
+    if hoard_key == 'files':
+        #code.interact(local=dict(globals(), **locals()))
+        hoard_archive[hoard_key][key].append(entry)
+    else:
+        hoard_archive[hoard_key].append(entry)
+    W_yml(hoard_archive_path, hoard_archive)
+
+
+#-----------------------------------------------------------------------------#
+#                                    MISC                                     #
+#-----------------------------------------------------------------------------#
+
+
+#=== Chars for copy
 """ Unicode math symbols
 ₀₁₂₃₄₅₆₇₈₉
 𝑤𝑢𝑣𝑥𝑦𝑧𝑠𝑡𝑝𝑞𝑛𝑚𝑓𝑔𝑖𝑗𝑘
@@ -42,183 +273,3 @@ import yaml
 ϶∘∙²³¹ϐ₊₋ₓ
 
 """
-
-# yaml rw helpers
-# ===============
-def R_yml(fname):
-    with open(fname) as file:
-        return yaml.load(file)
-
-def W_yml(fname, obj):
-    with open(fname, 'w') as file:
-        yaml.dump(obj, file, default_flow_style=False)
-
-
-# Decorators
-# ==========
-def NOTIMPLEMENTED(f):
-    """ Like TODO, but for functions in a class
-        raises error when wrappee is called """
-    @wraps(f)
-    def not_implemented(*args, **kwargs):
-        func_class = args[0]
-        f_class_name = func_class.get_class_name()
-        f_name = f.__name__
-        msg = '\n  Class: {}, function: {} has not been implemented!\n'
-        print(msg.format(f_class_name, f_name))
-        raise NotImplementedError
-    return not_implemented
-
-
-
-#=============================================================================#
-#                                       _     _                               #
-#                       _ __     __ _  | |_  | |__    ___                     #
-#                      | '_ \   / _` | | __| | '_ \  / __|                    #
-#                      | |_) | | (_| | | |_  | | | | \__ \                    #
-#                      | .__/   \__,_|  \__| |_| |_| |___/                    #
-#                      |_|                                                    #
-#                                                                             #
-#=============================================================================#
-
-###  BASE LEVEL  ###
-HOME  = os.environ['HOME']
-
-# Dot dirs
-# ========
-DOTS = f"{HOME}/.Dots"
-APPS = f"{HOME}/.Apps"
-BIN  = f"{HOME}/.local/bin"
-DESKTOPS = f"{HOME}/.local/share/applications"
-
-#-----------------------------------------------------------------------------#
-#                                 Home Paths                                  #
-#-----------------------------------------------------------------------------#
-#==== Home parents
-CHEST = f"{HOME}/Chest"
-CLOUD = f"{HOME}/Cloud"
-PROJECTS  = f"{HOME}/Projects"
-DOCUMENTS = f"{HOME}/Documents"
-
-#==== .apps
-ICONS = f"{APPS}/Icons"
-NATIVEFIED = f"{APPS}/Nativefied"
-BINARIES   = f"{APPS}/Binaries"
-
-#==== Cloud
-READMES = f"{CLOUD}/READMEs"
-AWESOME_LISTS = READMES + "/awesome_lists"
-RESOURCES_CLOUD = f"{CLOUD}/Resources"
-DOCUMENTS_CLOUD = f"{CLOUD}/Reading"
-
-#==== Chest
-DOTS_CHEST = f"{CHEST}/Dots"
-RESOURCES_CHEST = f"{CHEST}/Resources"
-
-#==== Projects
-HOARD = f"{PROJECTS}/Hoard"
-HOARD_ARCHIVE = HOARD + "/Archive"
-PAPER = f"{PROJECTS}/DocHub/Literature"
-PREP  = f"{PROJECTS}/Prep"
-
-# Files
-# =====
-
-# CURRENTLY UNUSED
-# ================
-"""
-MEDIA = f"{HOME}/Media"
-
-"""
-#=============================================================================#
-#                            __   _   _                                       #
-#                           / _| (_) | |   ___   ___                          #
-#                          | |_  | | | |  / _ \ / __|                         #
-#                          |  _| | | | | |  __/ \__ \                         #
-#                          |_|   |_| |_|  \___| |___/                         #
-#                                                                             #
-#=============================================================================#
-
-# fpaths
-inbox_read_path = f"{CLOUD}/Reading/inbox.txt"
-
-#==== resources
-api_tokens_path = f"{RESOURCES_CLOUD}/api_tokens.yml"
-zot_token = lambda: R_yml(api_tokens_path)['zotero']
-public_gh_tokens = lambda: R_yml(api_tokens_path)['github']
-
-def add_to_read_inbox(entry):
-    with open(inbox_read_path, 'a') as ibx:
-        ibx.write(entry + '\n')
-
-#=============================================================================#
-#           _                                  _   _                          #
-#          | |__     ___     __ _   _ __    __| | (_)  _ __     __ _          #
-#          | '_ \   / _ \   / _` | | '__|  / _` | | | | '_ \   / _` |         #
-#          | | | | | (_) | | (_| | | |    | (_| | | | | | | | | (_| |         #
-#          |_| |_|  \___/   \__,_| |_|     \__,_| |_| |_| |_|  \__, |         #
-#                                                              |___/          #
-#                                                                             #
-#=============================================================================#
-def init_github():
-    import github3
-    tokens = public_gh_tokens()
-    hoard_token = tokens['hoard']['token']
-    github = github3.login(token=hoard_token)
-    return github
-
-
-#-----------------------------------------------------------------------------#
-#                                 Hoard files                                 #
-#-----------------------------------------------------------------------------#
-# Paths
-hoard_inbox_path   = f"{HOARD}/inbox.yml"
-hoard_archive_path = f"{HOARD}/archive.yml"
-
-# Inbox/archive mapping
-hoard_inbox_keys = {'o': 'orgs', 'r': 'repos', 'u':'users'}
-
-# smelly mapping: 'files' keys are mapped in somewhat opposite manner
-#  to inbox keys, but it makes the archiving func a little cleaner
-hoard_archive_keys = {'awesome_lists': 'files',
-                      'readmes': 'files',
-                      'src': 'files',
-                      'gists': 'files',
-                      **hoard_inbox_keys}
-
-#-----------------------------------------------------------------------------#
-#                               hoard rw utils                                #
-#-----------------------------------------------------------------------------#
-
-# Hoard inboxing
-def add_to_inbox(key, entry):
-    assert key in hoard_inbox_keys
-    key = hoard_inbox_keys[key]
-    hoard = R_yml(hoard_inbox_path)
-    hoard[key].append(entry)
-    W_yml(hoard_inbox_path, hoard)
-
-# Hoard archive
-def add_to_archive(key, entry):
-    """
-    Two types of indexing
-    add_to_archive('src', <entry>)
-      key == 'src'; hoard_key == 'files'
-      hoard_archive[hoard_key][key] == hoard_archive['files']['src']
-
-    add_to_archive('r', <entry>)
-      key == 'r'; hoard_key == 'repos';
-      hoard_archive[hoard_key]
-
-    """
-    assert key in hoard_archive_keys
-    hoard_key = hoard_archive_keys[key]
-    hoard_archive = R_yml(hoard_archive_path)
-
-    if hoard_key == 'files':
-        #code.interact(local=dict(globals(), **locals()))
-        hoard_archive[hoard_key][key].append(entry)
-    else:
-        hoard_archive[hoard_key].append(entry)
-    W_yml(hoard_archive_path, hoard_archive)
-
