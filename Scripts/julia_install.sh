8f233a91d7c5c1e366e04b9d6af006b8c3e702fb
Evan Cushing <evdcush@protonmail.com>
2020-02-29 12:59:27 +0900

    completed install script julia

--- Scripts/julia_install.sh
+++ Scripts/julia_install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-# Script for installing Julia from tarball
+##  Script for installing Julia from tarball  ##
 
 
 # Version
@@ -8,4 +8,18 @@
 major='1.3'
 minor='1'
 VER="$major.$minor"
+
+# Formatting
+# ----------
+url="https://julialang-s3.julialang.org/bin/linux/x64/$major/julia-$VER-linux-x86_64.tar.gz"
+tball="julia-$VER-linux-x86_64.tar.gz"
+jdir="julia-$VER"
+
+
+# Get & install
+# =============
+wget $url
+tar -xvf $tball
+sudo mv $jdir /opt
+sudo ln -sf /opt/$jdir/bin/julia /usr/local/bin
 
