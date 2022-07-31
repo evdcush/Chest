91b7c3db34f9bfac434206cf398070111f965d67
Evan C <evdcush@protonmail.com>
2022-07-31 22:27:16 +0900

    Snippet for changing all instances in history of author with wrong email

--- Reference/git_snippets.sh
+++ Reference/git_snippets.sh
@@ -279,6 +279,34 @@ git branch -d <branch_name> # local
 #=============================================================================#
 
 
+
+#=============================================================================#
+#                             Changing Authorship                             #
+#=============================================================================#
+
+
+
+# For all commits in history
+# ==========================
+
+git filter-branch --env-filter '
+WRONG_EMAIL="foo.bar@A.com"
+NEW_NAME="Alice Bob"
+NEW_EMAIL="proper.email@foo.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$WRONG_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$NEW_NAME"
+    export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$WRONG_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$NEW_NAME"
+    export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+
+
 #=============================================================================#
 #                            __  __   _                                       #
 #                           |  \/  | (_)  ___    ___                          #
