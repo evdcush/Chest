482acb4ea7dff58c42e3ce7eca188a150f4d734e
Evan C <evdcush@protonmail.com>
2022-08-15 20:11:03 +0900

    jammy: sublime-text

--- Setup/jammy_setup.sh
+++ Setup/jammy_setup.sh
@@ -229,6 +229,14 @@ sudo apt install -y firefox-esr
 
 # Sublime
 # =======
+# Get the key.
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg
+
+# Add apt source.
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
+# Install.
+sudo apt update && sudo apt install -y sublime-text
 
 # VSCode
 # ======
