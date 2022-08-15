bfe1c217b524126f9ad6f91854456ebf9d0e706f
Evan C <evdcush@protonmail.com>
2022-08-15 19:55:03 +0900

    jammy: fonts

--- Setup/jammy_setup.sh
+++ Setup/jammy_setup.sh
@@ -126,6 +126,19 @@ gnome-user-docs-ja
 #                                                                             #
 #=============================================================================#
 
+# General
+# =======
+sudo apt install -y guake
+
+# Fonts
+# =====
+sudo apt install -y fonts-hack fonts-inter \ 
+fonts-cabin fonts-cantarell fonts-comfortaa fonts-ebgaramond \ 
+fonts-firacode fonts-font-awesome fonts-mathjax fonts-mathjax-extras fonts-mikachan \ 
+fonts-misaki fonts-moe-standard-kai fonts-mononoki fonts-motoya-l-cedar \ 
+fonts-motoya-l-maruberi fonts-mplus fonts-noto-cjk fonts-noto-color-emoji fonts-oxygen \ 
+fonts-roboto fonts-roboto-slab "fonts-sawarabi*" fonts-seto fonts-lato fonts-umeplus \ 
+fonts-ubuntu fonts-ubuntu-console fonts-vollkorn
 
 
 #=============================================================================#
