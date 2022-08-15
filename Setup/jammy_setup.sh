591223e24e1b23764ebe077490aa76d446c54071
Evan C <evdcush@protonmail.com>
2022-08-15 19:36:38 +0900

    jammy: fzf install

--- Setup/jammy_setup.sh
+++ Setup/jammy_setup.sh
@@ -55,6 +55,12 @@ PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/u
 # Oh-my-zsh
 # =========
 
+# fzf
+# ===
+# Install fzf:
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
 # pyenv
 # =====
 # (Assumes you installed deps above!)
