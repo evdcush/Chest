7142c4036a0668ebaaac3e632a2ef51f74f5ca78
Evan C <evdcush@protonmail.com>
2022-08-15 19:52:12 +0900

    jammy: install brave

--- Setup/jammy_setup.sh
+++ Setup/jammy_setup.sh
@@ -143,10 +143,12 @@ sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://b
 # Deb apt source.
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
+# Install.
+sudo apt update && sudo apt install -y brave-browser
+
 
 # Firefox
 # =======
-
 # First, get rid of snap shit (should have done this earlier).
 sudo apt remove --purge snapd
 
