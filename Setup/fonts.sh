710330c9d32e70c648b6c3c40d4924077fd702ee
Evan C <evdcush@protonmail.com>
2022-11-03 20:10:00 +0900

    fonts file

---
+++ Setup/fonts.sh
@@ -0,0 +1,33 @@
+
+
+sudo apt install -y \
+fonts-hack \
+fonts-inter \
+fonts-cabin \
+fonts-cantarell \
+fonts-comfortaa \
+fonts-ebgaramond \
+fonts-firacode \
+fonts-font-awesome \
+fonts-mathjax \
+fonts-mathjax-extras \
+fonts-mikachan \
+fonts-misaki \
+fonts-moe-standard-kai \
+fonts-mononoki \
+fonts-motoya-l-cedar \
+fonts-motoya-l-maruberi \
+fonts-mplus \
+fonts-noto-cjk \
+fonts-noto-color-emoji \
+fonts-oxygen \
+fonts-roboto \
+fonts-roboto-slab \
+"fonts-sawarabi*" \
+fonts-seto \
+fonts-lato \
+fonts-umeplus \
+fonts-ubuntu \
+fonts-ubuntu-console \
+fonts-vollkorn\
+
