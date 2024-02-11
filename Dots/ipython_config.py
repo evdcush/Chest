a114c262bacd9c941709b6245964d391fc19b58c
Evan C <evdcush@protonmail.com>
2024-02-11 18:59:01 +0900

    dots: ipy conf and some aliases

--- Dots/ipython_config.py
+++ Dots/ipython_config.py
@@ -42,7 +42,9 @@ c.InteractiveShellApp.exec_lines = [
 ]
 
 ## A list of dotted module names of IPython extensions to load.
-#c.InteractiveShellApp.extensions = []
+c.InteractiveShellApp.extensions = [
+    'rich',
+]
 
 ## dotted module name of an IPython extension to load.
 #c.InteractiveShellApp.extra_extension = ''
