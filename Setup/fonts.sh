43258d1a68cc2c054fe973a0e230e684a95c28dc
Evan C <evdcush@protonmail.com>
2026-03-14 20:05:16 +0900

    setup/fonts.sh: misc reference copypa recent fonts i guess

--- Setup/fonts.sh
+++ Setup/fonts.sh
@@ -63,3 +63,19 @@ sudo apt install -y typecatcher
 
 # Rebuild font database
 sudo fc-cache -f -v
+
+
+1.2M skill-icons-main.zip
+1.5M Screenshot_20250920_132652_Fennec.jpg
+1.7M Screenshot_20250920_221244_Fennec.png
+1.9M pixel-icon-library-by-hackernoon.zip
+2.0M Screenshot_20250920_131052_Fennec.jpg
+2.0M DotGothic16.zip
+2.3M Noto_Serif_Hentaigana,Tirra.zip
+3.1M Akatab,Noto_Serif_Hentaigana,Nuosu_SIL,Tirra.zip
+4.5M Screenshot_20250920_221007_Fennec.jpg
+ 27M Akatab,Noto_Sans_Anatolian_Hieroglyphs,Noto_Sans_Bamum,Noto_Sans_Buginese,Noto_Sans_Buhid,etc.zip
+ 71M Noto_Serif_JP.zip
+119M Aoboshi_One,Astloch,Bytesized,Chokokutai,Coral_Pixels,etc.zip
+
+Hentaigana
